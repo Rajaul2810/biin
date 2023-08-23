9e0e7d6316a959f04e40456632f48d8bec87e875
unknown <rajaulkarim2810@gmail.com>
2023-08-23 20:20:10 +0600

    debug

--- src/app/Components/Hero.jsx
+++ src/app/Components/Hero.jsx
@@ -1,52 +1,74 @@
-"use client"
-import React from 'react'
-import hero from '../../../public/hero.jpg'
-import Link from 'next/link'
-import Image from 'next/image'
-import { motion, spring } from 'framer-motion'
+"use client";
+import React from "react";
+import hero from "../../../public/hero.jpg";
+import Link from "next/link";
+import Image from "next/image";
+import { motion, spring } from "framer-motion";
 
 const Hero = () => {
-    return (
-        <section className='pt-10 pb-14 bg-gray-900 h-96'>
-            <div>
-                <div className=' flex justify-center'>
-                    <div className=' w-11/12 md:w-3/4 grid gap-2 grid-cols-1 md:grid-cols-2 place-items-center '>
-                        <div className=''>
-                            <motion.h1
-                            initial={{y:'2rem',opacity:0}}
-                            animate={{y:0,opacity:1}}
-                            transition={{
-                                duration:2,
-                                type:"spring"
-                            }}
-                             className='text-3xl sm:text-4xl text-center font-extrabold text-teal-500 pt-5'>Empowering Youth,</motion.h1>
-                            <motion.h1
-                            initial={{y:'2rem',opacity:0}}
-                            animate={{y:0,opacity:1}}
-                            transition={{
-                                duration:2,
-                                type:"spring"
-                            }}
-                            
-                            className='text-3xl sm:text-4xl text-center font-extrabold text-teal-500 pt-2'><span > Transforming Tomorrow</span></motion.h1>
-                            <p className=' text-gray-400 pt-5 pb-5 mb-5'>Bangladesh ICT & Innovation Network (BIIN) is an exemplary non-profit organization that stands as a beacon of hope for the youth of Bangladesh.</p>
-                            <p className='mb-5 text-white'><Link href="/contact" className='btn bg-gradient-to-r from-teal-600 to-teal-500 p-3  rounded-md hover:border-2 hover:border-white '>Contact Us</Link></p>
-                        </div>
-                        <motion.div
-                        initial={{x:'7rem',opacity:0}}
-                        animate={{x:0,opacity:1}}
-                        transition={{
-                            duration:2,
-                            type:"spring"
-                        }}
-                        >
-                            <Image src={hero} alt='man' height="100%" width='100%' className=' rounded-md' />
-                        </motion.div>
-                    </div>
-                </div>
+  return (
+    <section className="pt-10 pb-14 bg-gray-900 bg-cover">
+      <div>
+        <div className=" flex justify-center">
+          <div className=" w-11/12 md:w-3/4 grid gap-2 grid-cols-1 md:grid-cols-2 place-items-center ">
+            <div className="">
+              <motion.h1
+                initial={{ y: "2rem", opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{
+                  duration: 2,
+                  type: "spring",
+                }}
+                className="text-3xl sm:text-4xl text-center font-extrabold text-teal-500 pt-5"
+              >
+                Empowering Youth,
+              </motion.h1>
+              <motion.h1
+                initial={{ y: "2rem", opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{
+                  duration: 2,
+                  type: "spring",
+                }}
+                className="text-3xl sm:text-4xl text-center font-extrabold text-teal-500 pt-2"
+              >
+                <span> Transforming Tomorrow</span>
+              </motion.h1>
+              <p className=" text-gray-400 pt-5 pb-5 mb-5">
+                Bangladesh ICT & Innovation Network (BIIN) is an exemplary
+                non-profit organization that stands as a beacon of hope for the
+                youth of Bangladesh.
+              </p>
+              <p className="mb-5 text-white">
+                <Link
+                  href="/contact"
+                  className="btn bg-gradient-to-r from-teal-600 to-teal-500 p-3  rounded-md hover:border-2 hover:border-white "
+                >
+                  Contact Us
+                </Link>
+              </p>
             </div>
-        </section>
-    )
-}
+            <motion.div
+              initial={{ x: "7rem", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{
+                duration: 2,
+                type: "spring",
+              }}
+            >
+              <Image
+                src={hero}
+                alt="man"
+                height="100%"
+                width="100%"
+                className=" rounded-md"
+              />
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
