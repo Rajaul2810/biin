1e9426b7ebf635e68b67d63f0239823bcc8e66a9
unknown <rajaulkarim2810@gmail.com>
2023-08-28 00:45:54 +0600

    img

--- src/app/partner/page.jsx
+++ src/app/partner/page.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Marquee from "react-fast-marquee";
 import part from '../../../public/partner.png'
 import part1 from '../../../public/partner1.png'
-import part2 from '../../../public/partner2.png'
 import part3 from '../../../public/partner3.png'
 import part4 from '../../../public/partner4.png'
 import part5 from '../../../public/partner5.png'
@@ -17,7 +16,6 @@ const Partner = () => {
         <Marquee speed={80}>
            <Image src={part} alt='logo' className=' h-20 w-32 me-10 p-5'/>
            <Image src={part1} alt='logo' className=' h-20 w-32 me-10 p-5'/>
-           <Image src={part2} alt='logo' className=' h-20 w-32 me-10 p-5'/>
            <Image src={part3} alt='logo' className=' h-20 w-32 me-10 p-5'/>
            <Image src={part4} alt='logo' className=' h-20 w-32 me-10 p-5'/>
            <Image src={part5} alt='logo' className=' h-20 w-32 me-10 p-5'/>
@@ -27,7 +25,6 @@ const Partner = () => {
         <Marquee speed={80} direction='right'>
            <Image src={part} alt='logo' className=' h-20 w-32 me-10 p-5'/>
            <Image src={part1} alt='logo' className=' h-20 w-32 me-10 p-5'/>
-           <Image src={part2} alt='logo' className=' h-20 w-32 me-10 p-5'/>
            <Image src={part3} alt='logo' className=' h-20 w-32 me-10 p-5'/>
            <Image src={part4} alt='logo' className=' h-20 w-32 me-10 p-5'/>
            <Image src={part5} alt='logo' className=' h-20 w-32 me-10 p-5'/>
